566703c7ac927ba5eab64ef6d00b63f31421a481
Helga <fenix.2112@yandex.ru>
2014-11-03 13:48:41 +0200

    test app

---
+++ Assets/TestButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (">>");
+		UIButton button = UIButton.create ("buttonGeenDown.png","buttonGeenUp.png",0,0);
+	}
+
+}
